03df39cbd94a4a4c704a4c574a5d9b77864f2b53
Osanda Madugalle <osandamadugalle@gmail.com>
2025-09-28 12:29:03 +0530

    created settings screen

--- app/src/main/java/com/example/wellness_pro/ui/SettingsActivity.kt
+++ app/src/main/java/com/example/wellness_pro/ui/SettingsActivity.kt
@@ -1,22 +1,41 @@
 package com.example.wellness_pro.ui
 
 import android.os.Bundle
+import android.view.MenuItem // ADDED for onOptionsItemSelected
+import androidx.appcompat.widget.Toolbar // ADDED for Toolbar
 import com.example.wellness_pro.R
-import com.example.wellness_pro.navbar.BaseBottomNavActivity
+// import com.example.wellness_pro.navbar.BaseBottomNavActivity // REMOVED
+import com.example.wellness_pro.navbar.BaseActivity // CHANGED to BaseActivity
 
-class SettingsActivity : BaseBottomNavActivity() {
+class SettingsActivity : BaseActivity() { // CHANGED parent class
 
     override val layoutId: Int
         get() = R.layout.activity_settings
 
-    // If Settings will be a main navigation item, specify its ID here.
-    // If not, you might remove this or handle it differently.
-    override val currentNavControllerItemId: Int
-        get() = R.id.navButtonSettings // Assuming you have/will have an ID 'navButtonSettings' in your menu
+    // REMOVED: override val currentNavControllerItemId: Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_settings)
+        setSupportActionBar(toolbar)
+
+        // Enable the Up button (back arrow)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         // Specific initialization for SettingsActivity can go here
-        // The layout is set by BaseBottomNavActivity using layoutId
+    }
+
+    // ADDED: Handle Up button press
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed() // or finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
